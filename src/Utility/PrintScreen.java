2b248f3a55c8f163a042f5f1a62f94727a35b504
Tran Dang Bao Nhi <tr.baonhi3008@gmail.com>
2022-06-19 14:31:56 +0700

    Delete src directory

--- src/Utility/PrintScreen.java
+++
@@ -1,7 +0,0 @@
-package Utility;
-
-public class PrintScreen {
-    public static void print(Object t) {System.out.print(t);}
-    public static void println(Object t) {System.out.println(t);}
-    public static void printf(String s, Object t, Object o) {System.out.printf(s,t,o);}
-}
